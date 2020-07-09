8be13d2a0ca4a3afc1e72bb71c1afd7f87fcdd12
zjy7 <zjy7@hotmail.com>
2020-07-09 14:21:58 +0800

    uniq

--- src/views/myFont/index.vue
+++ src/views/myFont/index.vue
@@ -5,11 +5,31 @@
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
   data(){
     return {
       
     }
+  },
+  mounted(){
+    // debugger
+    // console.log(_)
+    // console.log(_.uniqWith)
+    let arr=[
+      {a:1,b:2},
+      {a:1},
+      {a:1},
+      {a:2}
+      ]
+    let brr = _.uniqWith(arr,(a,b)=>{
+      if(a.a===b.a){
+        return true
+      }else{
+        return false
+      }
+    })
+    console.log(brr) // output [{a:1,b:2},{a:2}]
   }
 }
 </script>
